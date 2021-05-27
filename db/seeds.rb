fd4cf477510f5bb66c10d23a493dc16975ce241f
Peter Ntiamoah <peterntiamoah@Peters-MacBook-Pro.local>
2021-05-27 02:47:18 -0400

    Lesson 7 mdb Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+show = Show.create(title: "Game of Thrones")
+show.actors
+show.actors.create(name: "Peter Dinklage")
+show.actors.create(name: "Lena Headey")
+#pp Show.first.actors
+Actor.first.production
+movie = Movie.create(title: "Fight Club")
+movie.actors.create(name: "Brad Pitt")
+movie.actors.create(name: "Edward Norton")
+Movie.first.actors
+#pp Actor.all
